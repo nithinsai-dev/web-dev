a38dfb800dc395c2298898d725ca4d6341c451e7
student-nithin <ninsai2007@gmail.com>
2026-03-06 15:33:14 +0530

    new project

---
+++ Minprojects/Dice Roller Game/script.js
@@ -0,0 +1,21 @@
+function rollDice() {
+
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var diceImageName1 = "./images/dice" + randomNumber1 + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", diceImageName1);
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var diceImageName2 = "./images/dice" + randomNumber2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", diceImageName2);
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    }
+    else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+    }
+    else {
+        document.querySelector("h1").innerHTML = "Draw 🤝";
+    }
+
+}
