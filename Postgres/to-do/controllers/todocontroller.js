64dc7c4cdef98ba92917b7a16d7a018974e9f2ee
student-nithin <ninsai2007@gmail.com>
2026-07-11 22:52:40 +0530

    initial setup models and controllers done

---
+++ Postgres/to-do/controllers/todocontroller.js
@@ -0,0 +1,108 @@
+import {
+    getTodos,
+    getTodoById,
+    createTodo,
+    updateTodo,
+    deleteTodo
+} from "../controllers/todocontroller.js"
+
+export const getAllTodo = async (req, res) => {
+    try {
+        const todos = await getTodos();
+
+        res.send(200).json(todos);
+    } catch (err) {
+        res.send(500).json({
+            message: err.message
+        });
+        console.log(err.message);
+    }
+};
+
+export const getTodo = async (req, res) => {
+    try {
+        const { id } = req.pramas;
+        const todo = await getTodoById(id);
+
+        if (!todo) {
+            res.send(400).json({
+                message: "todo not found"
+            })
+        }
+
+        res.send(200).json(todo);
+    } catch (err) {
+        res.send(500).json({
+            message: err.message
+        });
+    }
+};
+
+export const addTodo = async (req, res) => {
+
+    try {
+
+        const { title, description } = req.body;
+
+        const todo = await createTodo(title, description);
+
+        res.status(201).json(todo);
+
+    } catch (err) {
+
+        res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+};
+
+export const editTodo = async (req, res) => {
+
+    try {
+
+        const { id } = req.params;
+
+        const { title, description, is_completed } = req.body;
+
+        const todo = await updateTodo(
+            id,
+            title,
+            description,
+            is_completed
+        );
+
+        res.status(200).json(todo);
+
+    } catch (err) {
+
+        res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+};
+
+export const removeTodo = async (req, res) => {
+
+    try {
+
+        const { id } = req.params;
+
+        await deleteTodo(id);
+
+        res.status(200).json({
+            message: "Todo deleted successfully"
+        });
+
+    } catch (err) {
+
+        res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+};
